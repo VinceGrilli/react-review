d3020629ad2ed45246af32f59eb71e811793f05a
BrainJarProductions <vgrilli1@gmail.com>
2020-04-14 22:10:57 -0400

    start react review class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [monsters, setMonster] = useState([
+    {
+      name: 'Frankenstein',
+      id: '20394',
+    },
+    {
+      name: 'Dracula',
+      id: '02dd4',
+    },
+    {
+      name: 'Zombie',
+      id: '34234',
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>
+          {monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </div>
       </header>
     </div>
   );
