a0dadd75af260212f7097d7b5d6f1bd718d39626
BrainJarProductions <vgrilli1@gmail.com>
2020-04-19 14:32:54 -0400

    Updates

---
+++ precache-manifest.4117244379d48fa2429995474b3bcebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67391e08786644787204c3d43016a71",
+    "url": "/react-review/index.html"
+  },
+  {
+    "revision": "4d9497eae4aa64b44164",
+    "url": "/react-review/static/css/main.82e77f46.chunk.css"
+  },
+  {
+    "revision": "5816173c2b585a5f35d5",
+    "url": "/react-review/static/js/2.2ab060a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-review/static/js/2.2ab060a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9497eae4aa64b44164",
+    "url": "/react-review/static/js/main.1ac15de3.chunk.js"
+  },
+  {
+    "revision": "ea69d7aa24007dd18f41",
+    "url": "/react-review/static/js/runtime-main.2e6d8e01.js"
+  }
+]);
